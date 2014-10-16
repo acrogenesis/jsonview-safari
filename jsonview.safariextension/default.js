3ecf4955b28470579d0028b0d89b397e65f986d0
Adrian Rangel <adrian.rangel@gmail.com>
2014-10-16 18:56:34 -0500

    remove trailing white spaces

--- jsonview.safariextension/default.js
+++ jsonview.safariextension/default.js
@@ -3,13 +3,13 @@ document.addEventListener('DOMContentLoaded', function() {
   // Click handler for collapsing and expanding objects and arrays
   function collapse(evt) {
     var collapser = evt.target;
-   
+
     var target = collapser.parentNode.getElementsByClassName('collapsible');
-    
+
     if ( ! target.length ) {
       return;
     }
-    
+
     target = target[0];
 
     if ( target.style.display == 'none' ) {
@@ -18,29 +18,29 @@ document.addEventListener('DOMContentLoaded', function() {
       target.style.display = '';
     } else {
       target.style.display = 'none';
-      
+
       var ellipsis = document.createElement('span');
       ellipsis.className = 'ellipsis';
       ellipsis.innerHTML = ' &hellip; ';
       target.parentNode.insertBefore(ellipsis, target);
     }
-    
+
     collapser.innerHTML = ( collapser.innerHTML == '-' ) ? '+' : '-';
   }
-  
+
   function addCollapser(item) {
     // This mainly filters out the root object (which shouldn't be collapsible)
     if ( item.nodeName != 'LI' ) {
       return;
     }
-    
+
     var collapser = document.createElement('div');
     collapser.className = 'collapser';
     collapser.innerHTML = '-';
     collapser.addEventListener('click', collapse, false);
     item.insertBefore(collapser, item.firstChild);
   }
-  
+
   var items = document.getElementsByClassName('collapsible');
   for( var i = 0; i < items.length; i++) {
     addCollapser(items[i].parentNode);
