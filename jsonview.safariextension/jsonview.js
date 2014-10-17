c6e925179ccd9a4606b5424408fe3a7028dec072
Adrian Rangel <adrian.rangel@gmail.com>
2014-10-16 19:59:41 -0500

    update jsonview and default, fix collapse

--- jsonview.safariextension/jsonview.js
+++ jsonview.safariextension/jsonview.js
@@ -8,139 +8,196 @@ if(/^\<pre.*\>(.*)\n?\<\/pre\>$/.test(this.data)){
 
 var json_regex = /^\s*([\[\{].*[\}\]])\s*$/; // Ghetto, but it works
 var jsonp_regex = /^[\s\u200B\uFEFF]*([\w$\[\]\.]+)[\s\u200B\uFEFF]*\([\s\u200B\uFEFF]*([\[{][\s\S]*[\]}])[\s\u200B\uFEFF]*\);?[\s\u200B\uFEFF]*$/;
-var jsonp_regex2 = /([\[\{][\s\S]*[\]\}])\);/ // more liberal support... this allows us to pass the jsonp.json & jsonp2.json tests
+var jsonp_regex2 = /([\[\{][\s\S]*[\]\}])\)/; // more liberal support... this allows us to pass the jsonp.json & jsonp2.json tests
 var is_json = json_regex.test(this.data);
 var is_jsonp = jsonp_regex.test(this.data);
 // console.log("JSONView: is_json="+is_json+" is_jsonp="+is_jsonp);
 
 if(is_json || is_jsonp){
-
-  // console.log("JSONView: sexytime!");
-  // JSONFormatter json->HTML prototype straight from Firefox JSONView
-  // For reference: http://code.google.com/p/jsonview
-  function JSONFormatter() {
-    // No magic required.
-  }
+  /*
+   * The JSONFormatter helper object. This contains two major functions, jsonToHTML and errorPage,
+   * each of which returns an HTML document.
+   */
+  function JSONFormatter() { }
 
   JSONFormatter.prototype = {
-    htmlEncode: function (t) {
-      return t != null ? t.toString().replace(/&/g,"&amp;").replace(/"/g,"&quot;").replace(/</g,"&lt;").replace(/>/g,"&gt;") : '';
+    /**
+     * Encode a string to be used in HTML
+     */
+    htmlEncode: function(t) {
+      return t !== null ? t.toString().replace(/&/g,"&amp;").replace(/"/g,"&quot;").replace(/</g,"&lt;").replace(/>/g,"&gt;") : '';
     },
 
-    decorateWithSpan: function (value, className) {
+    /**
+     * Completely escape a json string
+     */
+    jsString: function(s) {
+      // Slice off the surrounding quotes
+      s = JSON.stringify(s).slice(1, -1);
+      return this.htmlEncode(s);
+    },
+
+    /**
+     * Is this a valid "bare" property name?
+     */
+    isBareProp: function(prop) {
+      return /^[A-Za-z_$][A-Za-z0-9_\-$]*$/.test(prop);
+    },
+
+    /**
+     * Surround value with a span, including the given className
+     */
+    decorateWithSpan: function(value, className) {
       return '<span class="' + className + '">' + this.htmlEncode(value) + '</span>';
     },
 
     // Convert a basic JSON datatype (number, string, boolean, null, object, array) into an HTML fragment.
-    valueToHTML: function(value) {
+    valueToHTML: function(value, path) {
       var valueType = typeof value;
 
-      var output = "";
-      if (value == null) {
-        output += this.decorateWithSpan('null', 'null');
+      if (value === null) {
+        return this.decorateWithSpan('null', 'null');
       }
-      else if (value && value.constructor == Array) {
-        output += this.arrayToHTML(value);
+      else if (Array.isArray(value)) {
+        return this.arrayToHTML(value, path);
       }
       else if (valueType == 'object') {
-        output += this.objectToHTML(value);
+        return this.objectToHTML(value, path);
       }
       else if (valueType == 'number') {
-        output += this.decorateWithSpan(value, 'num');
+        return this.decorateWithSpan(value, 'num');
       }
       else if (valueType == 'string') {
-        if (/^(http|https):\/\/[^\s]+$/.test(value)) {
-          value = this.htmlEncode(value);
-          output += '<a href="' + value + '">' + value + '</a>';
+        if (/^(http|https|file):\/\/[^\s]+$/i.test(value)) {
+          return '<a href="' + this.htmlEncode(value) + '"><span class="q">&quot;</span>' + this.jsString(value) + '<span class="q">&quot;</span></a>';
         } else {
-          output += this.decorateWithSpan('"' + value + '"', 'string');
+          return '<span class="string">&quot;' + this.jsString(value) + '&quot;</span>';
         }
       }
       else if (valueType == 'boolean') {
-        output += this.decorateWithSpan(value, 'bool');
+        return this.decorateWithSpan(value, 'bool');
       }
 
-      return output;
+      return '';
     },
 
     // Convert an array into an HTML fragment
-    arrayToHTML: function(json) {
-      var output = '[<ul class="array collapsible">';
-      var hasContents = false;
-      for ( var prop in json ) {
-        hasContents = true;
-        output += '<li>';
-        output += this.valueToHTML(json[prop]);
+    arrayToHTML: function(json, path) {
+      if (json.length === 0) {
+        return '[ ]';
+      }
+
+      var output = '';
+      for (var i = 0; i < json.length; i++) {
+        var subPath = path + '[' + i + ']';
+        output += '<li>' + this.valueToHTML(json[i], subPath);
+        if (i < json.length - 1) {
+          output += ',';
+        }
         output += '</li>';
       }
-      output += '</ul>]';
-
-      if ( ! hasContents ) {
-        output = "[ ]";
-      }
-
-      return output;
+      return '<span class="collapser"></span>[<ul class="array collapsible">' + output + '</ul>]';
     },
 
     // Convert a JSON object to an HTML fragment
-    objectToHTML: function(json) {
-      var output = '{<ul class="obj collapsible">';
-      var hasContents = false;
-      for ( var prop in json ) {
-        hasContents = true;
-        output += '<li>';
-        output += '<span class="prop">' + this.htmlEncode(prop) + '</span>: ';
-        output += this.valueToHTML(json[prop]);
+    objectToHTML: function(json, path) {
+      var numProps = Object.keys(json).length;
+      if (numProps === 0) {
+        return '{ }';
+      }
+
+      var output = '';
+      for (var prop in json) {
+        var subPath = '';
+        var escapedProp = JSON.stringify(prop).slice(1, -1);
+        var bare = this.isBareProp(prop);
+        if (bare) {
+          subPath = path + '.' + escapedProp;
+        } else {
+          escapedProp = '"' + escapedProp + '"';
+        }
+        output += '<li><span class="prop' + (bare ? '' : ' quoted') + '" title="' + this.htmlEncode(subPath) +
+          '"><span class="q">&quot;</span>' + this.jsString(prop) +
+          '<span class="q">&quot;</span></span>: ' + this.valueToHTML(json[prop], subPath);
+        if (numProps > 1) {
+          output += ',';
+        }
         output += '</li>';
-      }
-      output += '</ul>}';
-
-      if ( ! hasContents ) {
-        output = "{ }";
+        numProps--;
       }
 
-      return output;
+      return '<span class="collapser"></span>{<ul class="obj collapsible">' + output + '</ul>}';
     },
 
-    // Convert a whole JSON object into a formatted HTML document.
+    // Convert a whole JSON value / JSONP response into a formatted HTML document
     jsonToHTML: function(json, callback, uri) {
-      var output = '';
-      if( callback ){
-        output += '<div class="callback">' + callback + ' (</div>';
-        output += '<div id="json">';
-      }else{
-        output += '<div id="json">';
-      }
-      output += this.valueToHTML(json);
-      output += '</div>';
-      if( callback ){
-        output += '<div class="callback">)</div>';
+      var output = '<div id="json">' + this.valueToHTML(json, '<root>') + '</div>';
+      if (callback) {
+        output = '<div class="callback">' + callback + '(</div>' + output + '<div class="callback">)</div>';
       }
       return this.toHTML(output, uri);
     },
 
+    // Clean up a JSON parsing error message
+    massageError: function(error) {
+      var message = error.message.replace(/^JSON.parse: /, '').replace(/of the JSON data/, '');
+      var parts = /line (\d+) column (\d+)/.exec(message);
+
+      return {
+        message: this.htmlEncode(message),
+        line: +parts[1],
+        column: +parts[2]
+      };
+    },
+
+    highlightError: function(data, lineNum, columnNum) {
+      if (!lineNum || !columnNum) {
+        return this.htmlEncode(data);
+      }
+
+      var lines = data.match(/^.*((\r\n|\n|\r)|$)/gm);
+
+      var output = '';
+      for (var i = 0; i < lines.length; i++) {
+        var line = lines[i];
+
+        if (i == lineNum - 1) {
+          output += '<span class="errorline">';
+          output += this.htmlEncode(line.substring(0, columnNum - 1)) + '<span class="errorcolumn">' + this.htmlEncode(line[columnNum - 1]) + '</span>' + this.htmlEncode(line.substring(columnNum));
+          output += '</span>';
+        } else {
+          output += this.htmlEncode(line);
+        }
+      }
+
+      return output;
+    },
+
     // Produce an error document for when parsing fails.
     errorPage: function(error, data, uri) {
-      // var output = '<div id="error">' + this.stringbundle.GetStringFromName('errorParsing') + '</div>';
-      // output += '<h1>' + this.stringbundle.GetStringFromName('docContents') + ':</h1>';
-      var output = '<div id="error">Error parsing JSON: '+error.message+'</div>';
-      output += '<h1>'+error.stack+':</h1>';
-      output += '<div id="json">' + this.htmlEncode(data) + '</div>';
+      // Escape unicode nulls
+      data = data.replace("\u0000","\uFFFD");
+
+      var errorInfo = this.massageError(error);
+
+      var output = '<div id="error">' + _('errorParsing');
+      if (errorInfo.message) {
+        output += '<div class="errormessage">' + errorInfo.message + '</div>';
+      }
+      output += '</div><div id="json">' + this.highlightError(data, errorInfo.line, errorInfo.column) + '</div>';
       return this.toHTML(output, uri + ' - Error');
     },
 
     // Wrap the HTML fragment in a full document. Used by jsonToHTML and errorPage.
     toHTML: function(content, title) {
-      return '<doctype html>' +
-        '<html><head><title>' + title + '</title>' +
-        '<link rel="stylesheet" type="text/css" href="'+safari.extension.baseURI + "default.css" +'">' +
-        '<script type="text/javascript" src="'+safari.extension.baseURI + "default.js" +'"></script>' +
+      return '<!DOCTYPE html>\n' +
+        '<html><head><title>' + this.htmlEncode(title) + '</title>' +
+        '<link rel="stylesheet" type="text/css" href="' + safari.extension.baseURI + "default.css" + '">' +
         '</head><body>' +
         content +
         '</body></html>';
     }
   };
-
 
   // Sanitize & output -- all magic from JSONView Firefox
   this.jsonFormatter = new JSONFormatter();
@@ -186,10 +243,71 @@ if(is_json || is_jsonp){
     // console.log(e);
     outputDoc = this.jsonFormatter.errorPage(e, this.data, this.uri);
   }
-  var links = '<link rel="stylesheet" type="text/css" href="'+safari.extension.baseURI + "default.css" +'">' +
-              '<script type="text/javascript" src="'+safari.extension.baseURI + "default.js" +'"></script>';
-  document.body.innerHTML = links + outputDoc;
-}
-else {
+  document.body.innerHTML = outputDoc;
+
+  //////////////////////////////////
+  ////////BEGINS DEFAULT.JS/////////
+  //////////////////////////////////
+
+  // Click handler for collapsing and expanding objects and arrays
+  function collapse(evt) {
+    var collapser = evt.target;
+
+    while (collapser && (!collapser.classList || !collapser.classList.contains('collapser'))) {
+      collapser = collapser.nextSibling;
+    }
+    if (!collapser || !collapser.classList || !collapser.classList.contains('collapser')) {
+      return;
+    }
+
+    evt.stopPropagation();
+
+    collapser.classList.toggle('collapsed');
+
+    var collapsible = collapser;
+    while (collapsible && (!collapsible.classList || !collapsible.classList.contains('collapsible'))) {
+      collapsible = collapsible.nextSibling;
+    }
+    collapsible.classList.toggle('collapsed');
+  }
+
+  /*
+   * Collapses the whole json using keyboard
+   * TODO: Add a navigator support for each of the elements
+   */
+  function collapseAll(evt) {
+    var inputList;
+
+    // Ignore anything paired with a modifier key. See https://github.com/bhollis/jsonview/issues/69
+    if (evt.ctrlKey || evt.shiftKey || evt.altKey || evt.metaKey) {
+      return;
+    }
+
+    if (evt.keyCode === 37) {  // Collapses the json on left arrow key up
+      inputList = document.querySelectorAll('.collapsible, .collapser');
+      for (var i = 0; i < inputList.length; i++) {
+        if (inputList[i].parentNode.id != 'json') {
+          inputList[i].classList.add('collapsed');
+        }
+      }
+      evt.preventDefault();
+    } else if (evt.keyCode === 39) { // Expands the json on right arrow key up
+      inputList = document.querySelectorAll('.collapsed');
+      for (var i = 0; i < inputList.length; i++) {
+        inputList[i].classList.remove('collapsed');
+      }
+      evt.preventDefault();
+    }
+  }
+
+  // collapse with event delegation
+  document.addEventListener('click', collapse, false);
+  document.addEventListener('keyup', collapseAll, false);
+
+  //////////////////////////////////
+  /////////ENDS DEFAULT.JS//////////
+  //////////////////////////////////
+
+}else {
   // console.log("JSONView: this is not json, not formatting.");
 }
