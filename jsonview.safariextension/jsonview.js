a541445b2d7b7c124d6d7d5919cd9b3bcd543963
Adrian Rangel <adrian.rangel@gmail.com>
2015-05-08 18:50:07 -0500

    use JSON.parse to check for valid json

--- jsonview.safariextension/jsonview.js
+++ jsonview.safariextension/jsonview.js
@@ -6,14 +6,18 @@ if(document.getElementsByTagName("pre")[0]){
   this.data = document.getElementsByTagName("pre")[0].innerHTML;
 }
 
-var json_regex = /^\s*([\[\{].*[\}\]])\s*$/; // Ghetto, but it works
-var jsonp_regex = /^[\s\u200B\uFEFF]*([\w$\[\]\.]+)[\s\u200B\uFEFF]*\([\s\u200B\uFEFF]*([\[{][\s\S]*[\]}])[\s\u200B\uFEFF]*\);?[\s\u200B\uFEFF]*$/;
-var jsonp_regex2 = /([\[\{][\s\S]*[\]\}])\)/; // more liberal support... this allows us to pass the jsonp.json & jsonp2.json tests
-var is_json = json_regex.test(this.data);
-var is_jsonp = jsonp_regex.test(this.data);
-// console.log("JSONView: is_json="+is_json+" is_jsonp="+is_jsonp);
+function IsJsonString(str) {
+  try {
+      JSON.parse(str);
+  } catch (e) {
+      return false;
+  }
+  return true;
+}
 
-if(is_json || is_jsonp){
+var is_json = IsJsonString(this.data);
+
+if(is_json){
   /*
    * The JSONFormatter helper object. This contains two major functions, jsonToHTML and errorPage,
    * each of which returns an HTML document.
